3d40fb7b54cb75db3efe2ba97c3a5ea825714556
teteu2703 <t3t3u0327@gmail.com>
2024-08-20 12:36:22 -0300

    aula do dia 20/08

--- api.py
+++ api.py
@@ -1,6 +1,19 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///livros.db'
+
+db = SQLAlchemy(app)
+
+class Livro(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(100), nullable=False)
+    autor = db.Column(db.String(100), nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 livros = [
     {'id': 1, 'titulo': '1984', 'autor': 'George Orwell'},
